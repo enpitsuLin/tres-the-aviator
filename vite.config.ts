3a5e6f87c2f0c2a5d94ece87cd578cdb0f4924e3
enpitsulin <enpitsulin@gmail.com>
2023-08-15 14:21:00 +0800

    chore: remove local alias

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,4 @@ import { templateCompilerOptions } from '@tresjs/core'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(templateCompilerOptions)],
-  resolve: {
-    alias: {
-      '@tresjs/core': '/home/enpitsulin/projects/tres/src/index.ts',
-    },
-  },
 })
