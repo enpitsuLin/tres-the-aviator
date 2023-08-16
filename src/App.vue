dca9b7cac2eb2450a89590a49840ade6c2562539
enpitsulin <enpitsulin@gmail.com>
2023-08-16 11:29:55 +0800

    feat: camera and fog

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core'
-import { OrbitControls } from '@tresjs/cientos'
 import Game from './components/Game.vue'
 import { useGame } from './composables/useGame'
 
@@ -9,12 +8,10 @@ const { game } = useGame()
 
 <template>
   <TresCanvas clear-color="gray" window-size shadow>
-    <OrbitControls />
     <TresPerspectiveCamera
       :position="[0, 200, game.planeDefaultHeight]"
-      :look-at="[0, 0, 0]"
     />
-    <!-- <TresFog :args="[0xF7D9AA, 100, 950]" /> -->
+    <TresFog :args="[0xF7D9AA, 100, 950]" />
     <Game />
   </TresCanvas>
 </template>
